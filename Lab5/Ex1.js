24a73755a78b9d9ccaab0c7c4880184a9f38a295
D. Port Me <dport@hawaii.edu>
2024-02-01 13:18:48 -1000

    xxx

---
+++ Lab5/Ex1.js
@@ -0,0 +1,8 @@
+age =  50;
+fav_num = 42;
+day_of_birth = 14;
+month_of_birth = 5;
+console.log(
+    age + fav_num / day_of_birth * month_of_birth,
+    (age + fav_num) / day_of_birth * month_of_birth
+    );
