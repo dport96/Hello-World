0dbc43270b2b79b084d579247c388a744114b39f
D. Port Me <dport@hawaii.edu>
2024-02-15 13:17:59 -1000

    xxx

--- Lab7/Ex2a.js
+++ Lab7/Ex2a.js
@@ -1,8 +1,11 @@
 require("./products_data.js");
 const num_products = 5;
 let product_number = 1;
-while(product_number <= num_products/2) {
+while(product_number <= num_products) {
     console.log(`${product_number}. ${ eval('name' + product_number)}`);
     product_number++;
+    if(product_number > num_products/2) {
+        console.log(`That's enough!`);
+        break;
+    }
 }
-console.log(`That's all we have!`);
---
+++ Lab7/Ex2b.js
@@ -0,0 +1,11 @@
+require("./products_data.js");
+const num_products = 5;
+let product_number = 0;
+while(++product_number <= num_products) {
+    if( (product_number > .25*num_products) && (product_number < .75*num_products) ) {
+        console.log(`${eval('name' + product_number)} is sold out!`);
+        continue;
+    }
+    console.log(`${product_number}. ${ eval('name' + product_number)}`);
+}
+console.log(`That's all we have!`);
